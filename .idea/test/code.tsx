7fb5ff1839eba6a1fc407bab85314118483b6437
19928 <1992898402@qq.com>
2025-03-08 22:08:49 +0800

    [前端:新增了一个test文件夹和一个code.tsx，写了一个按钮]:陈垣向

---
+++ .idea/test/code.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { Button } from '@nextui-org/react';
+import React, { useState } from 'react';
+
+export default function login()
+{
+    return(
+        <Button 
+        className="mb-3 w-full py-3 text-white text-lg rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+      >
+        一个按钮
+      </Button>
+    )
+}
